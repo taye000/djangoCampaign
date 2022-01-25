76e6d4b70c24e5a74c2afdff97b1ec255a0fa525
taye000 <taylorgitari@gmail.com>
2022-01-25 16:17:40 +0300

    initial

---
+++ campaigns/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.template.defaultfilters import slugify
+
+
+class Campaign(models.Model):
+    title = models.CharField(max_length=200)
+    descriptions = models.TextField()
+    slug = models.models.Slugfield(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    logo = CloudinaryField('Image', overwrite=True, format='jpg')
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        to_assign = slugify(self.title)
+
+        if Campaign.objects.filter(slug=to_assign).exists():
+            to_assign = to_assign+str(Campaign.objects.all().count())
+
+        self.slug = to_assign
+
+        super().save(*args, **kwargs)
+
+
+class Subscriber(models.Model):
+    campaign = models.ForeignKey(to=Campaign, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+        def __str__(self):
+            return self.email
