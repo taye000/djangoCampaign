8ee071ae9dccecbcd197aabf9d56d4ca23c4b45d
taye000 <taylorgitari@gmail.com>
2022-01-25 16:29:28 +0300

    second commit

--- campaigns/models.py
+++ campaigns/models.py
@@ -6,7 +6,7 @@ from django.template.defaultfilters import slugify
 class Campaign(models.Model):
     title = models.CharField(max_length=200)
     descriptions = models.TextField()
-    slug = models.models.Slugfield(max_length=255)
+    slug = models.SlugField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     logo = CloudinaryField('Image', overwrite=True, format='jpg')
